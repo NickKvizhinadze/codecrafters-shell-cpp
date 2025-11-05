5f2b4214470c58991d4969c697ecaa9ea5a132ad
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2025-11-05 19:44:11 +0400

    Added type command

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,42 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+int main()
+{
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  while (true)
-  {
-    std::cout << "$ ";
-
-    std::string command;
-    std::getline(std::cin, command);
-
-    if (command == "exit 0")
-      return 0;
-
-    if (command == "exit 1")
-      return 1;
-
-    if (command.starts_with("echo"))
+    while (true)
     {
-      std::cout << command.substr(5, command.length() - 5) << std::endl;
-      continue;
-    }
+        std::cout << "$ ";
 
-    std::cout << command << ": command not found" << std::endl;
-  }
+        std::string command;
+        std::getline(std::cin, command);
+
+        if (command == "exit 0")
+            return 0;
+
+        if (command == "exit 1")
+            return 1;
+
+        if (command.starts_with("echo"))
+        {
+            std::cout << command.substr(5, command.length() - 5) << std::endl;
+            continue;
+        }
+
+        if (command.starts_with("type"))
+        {
+            command = command.substr(5, command.length() - 5);
+            if (command == "exit" || command == "echo" || command == "type")
+                std::cout << command << " is a shell builtin" << std::endl;
+            else
+                std::cout << command << " : not found" << std::endl;
+
+            continue;
+        }
+
+        std::cout << command << ": command not found" << std::endl;
+    }
 }
