9a319cf5791b5482f8006294fa46c40a4da055a3
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2025-11-05 17:48:22 +0400

    Added exit command

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@ int main() {
     std::string command;
     std::getline(std::cin, command);
 
+    if (command == "exit")
+      return 0;
+
     std::cout << command << ": command not found" << std::endl;
   }
 }
